5935521d65390d6837bcc6019e6fbe40989fc66c
ssavutu <ssavutu@gmail.com>
2023-08-03 00:46:53 -0400

    test

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@ void on_center_button() {
 	static bool pressed = false;
 	pressed = !pressed;
 	if (pressed) {
-		pros::lcd::set_text(2, "I was pressed!");
+		//pros::lcd::set_text(2, "I was pressed!");
 	} else {
 		pros::lcd::clear_line(2);
 	}
